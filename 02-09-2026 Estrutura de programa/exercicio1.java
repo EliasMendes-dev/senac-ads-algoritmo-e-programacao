640d7963d347950ad48ba16d31fb73cc4089b7f8
José Elias Herminio Mendes <eliasmendes2908@gmail.com>
2026-09-02 11:13:41 -0300

    feat: retirando exercicio1.java

--- 02-09-2026 Estrutura de programa/exercicio1.java
+++
@@ -1,7 +0,0 @@
-public class exercicio1 {
-    public static void main(String[] args) {
-        System.out.println("Meu nome é José Elias Hermínio Mendes");
-        System.out.println("Meu RA é ");
-
-    }
-}
