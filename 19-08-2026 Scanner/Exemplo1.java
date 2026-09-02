40280d970a94e83f98b057eaf4234a231f23807c
José Elias Herminio Mendes <eliasmendes2908@gmail.com>
2026-09-02 08:21:37 -0300

    Adiciona exemplos de programas em Java para interação com o usuário e manipulação de dados

---
+++ 19-08-2026 Scanner/Exemplo1.java
@@ -0,0 +1,11 @@
+public class Exemplo1 {
+    public static void main(String[] args) {
+        System.out.println("Esse algoritmo vai listar meus 5 livros favoritos.\n");
+
+        System.out.println("1-) O Horror de Dunwich");
+        System.out.println("2-) Dagon");
+        System.out.println("3-) O Chamado de Cthulhu");
+        System.out.println("4-) A Pirâmide Vermelha");
+        System.out.println("5-) O Trono de Fogo");
+    }
+}
