652dc4a7b15140119e57d2b62f888913f5dc45e1
Zubizeru <jose.ehmendes@senacsp.edu.br>
2026-09-16 10:26:08 -0300

    Remove unused suppression warning in DesvioCondicional.java

--- 09-09-2026 Desvios Condicionais/DesvioCondicional.java
+++ 09-09-2026 Desvios Condicionais/DesvioCondicional.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 class DesvioCondicional {
     static Scanner scanner = new Scanner(System.in);
 
-    @SuppressWarnings("unused")
     public static void main(String[] args) {
         
         exercicio1();
